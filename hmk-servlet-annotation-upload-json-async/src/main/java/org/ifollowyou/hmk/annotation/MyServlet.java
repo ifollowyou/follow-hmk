cc039352f01576988c11341b5f42eacfe455bff5
ifollowyou <ifollowyou@aliyun.com>
2015-12-21 12:08:05 +0800

    add webservlet annotation

---
+++ hmk-servlet-annotation-upload-json-async/src/main/java/org/ifollowyou/hmk/annotation/MyServlet.java
@@ -0,0 +1,30 @@
+
+package org.ifollowyou.hmk.annotation;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(urlPatterns = {"/servlet"})
+public class MyServlet extends HttpServlet {
+
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=UTF-8");
+        try {
+            PrintWriter out = response.getWriter();
+            out.println("<h2>Hello @WebServlet</h2>");
+        } catch (IOException ioe) {
+
+        }
+    }
+
+
+}
